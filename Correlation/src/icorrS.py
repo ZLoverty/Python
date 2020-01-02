1ad9bf2f5f35853bfbdd345958d2c7d490129bd8
Zhengyang Liu <liuzy19911112@gmail.com>
2020-01-02 16:36:42 -0600

    1

--- Correlation/src/icorrS.py
+++ Correlation/src/icorrS.py
@@ -1,17 +1,14 @@
 import sys
 import os
+import time
 from corrLib import corrS, corrI, divide_windows, distance_corr, corrIseq
-# import numpy
-# folder = sys.argv[1]
-# wsize = sys.argv[2]
-# step = sys.argv[3]
-folder = r'I:\Github\Python\Correlation\test_images\boxsize_effect'
-wsize = 50
-step = 50
+import numpy
+folder = sys.argv[1]
+wsize = sys.argv[2]
+step = sys.argv[3]
 t1 = time.monotonic()    
 data_seq = corrIseq(folder, windowsize=[wsize, wsize], step=step)
 data_seq.to_csv(os.path.join(folder, 'Icorrdata.dat'), index=False)
 t2 = time.monotonic()
 t = (t2 - t1) / 3600
 print('Wall time: %.2f h' % t)
-# print(1)
