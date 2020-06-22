6902217178eea57304e1d73c4c23da6a917c5ab5
Zhengyang Liu <liuzy19911112@gmail.com>
2020-06-22 10:19:40 -0500

    update div2.py
    To include divcn divcv and divv in the result, document has been updated as well.

--- Correlation/src/py_files/div2.py
+++ Correlation/src/py_files/div2.py
@@ -10,7 +10,7 @@ import pandas as pd
 import time
 
 piv_folder = sys.argv[1]
-imgDir = sys.argv[2]
+folder_img = sys.argv[2]
 output_folder = sys.argv[3]
 winsize = int(sys.argv[4])
 step = int(sys.argv[5])
@@ -28,15 +28,15 @@ with open(os.path.join(output_folder, 'log.txt'), 'w') as f:
 
 count = 0
 ld = corrLib.readdata(piv_folder)
-img = io.imread(imgDir)
+
 for num, i in ld.iterrows():
     pivData = pd.read_csv(os.path.join(piv_folder, i.Dir))
     folder, file = os.path.split(i.Dir)
-    name_ind = file.find('-')
-    name = file[0: name_ind]
+    name = file.split('-')[0]
+    img = io.imread(os.path.join(folder_img, name+'.tif'))
     c, v, divcn, divcv, divv = corrLib.div_field(img, pivData, winsize, step)
     X, Y, I = corrLib.divide_windows(img, windowsize=[winsize, winsize], step=step)
-    data = pd.DataFrame().assign(x=X.flatten(), y=Y.flatten(), div=divv.flatten())
+    data = pd.DataFrame().assign(x=X.flatten(), y=Y.flatten(), divcn=divcn.flatten(), divcv=divcv.flatten(), divv=divv.flatten())
     data.to_csv(os.path.join(output_folder, i.Name+'.csv'), index=False)
     with open(os.path.join(output_folder, 'log.txt'), 'a') as f:
         f.write(time.asctime() + ' // ' + name + ' calculated\n')
@@ -46,13 +46,13 @@ python div2.py piv_folder imgDir output_folder winsize step
 """
         
 """ TEST PARAMS
-piv_folder = E:\Google Drive\data_share\Dynamics_raw\concentration_velocity_field\piv_result_50\80
-imgDir = E:\Google Drive\data_share\Dynamics_raw\processed_image\80_bp\900.tif
-output_folder = E:\Github\Python\Correlation\test_images\div\div2test
+piv_folder = I:\Github\Python\Correlation\test_images\div
+folder_img = I:\Github\Python\Correlation\test_images\div
+output_folder = I:\Github\Python\Correlation\test_images\div\div2result
 winsize = 50
 step = 25
 """
 
 """ LOG 3 sec / frame
-Tue Apr 21 20:09:27 2020 // 900 calculated
+Mon Jun 22 10:15:13 2020 // 900 calculated
 """
