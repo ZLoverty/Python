6d51574179ad68cccfa94e997b69e6ca27523a41
Zhengyang Liu <liuzy19911112@gmail.com>
2020-06-23 20:56:05 -0500

    update overall_intensity doc

--- Correlation/src/py_files/overall_intensity.py
+++ Correlation/src/py_files/overall_intensity.py
@@ -28,7 +28,23 @@ for num, i in l.iterrows():
     img = io.imread(i.Dir)
     t.append(num)
     I.append(img.mean())
+    with open(os.path.join(folder_out, 'log.txt'), 'a') as f:
+        f.write(time.asctime() + ' // image {} computed\n'.format(i.Name))
 data = pd.DataFrame().assign(t=t, intensity=I)
 data.to_csv(os.path.join(folder_out, 'intensity.csv'))
 
+""" SYNTAX
+python overall_intensity.py folder folder_out
+"""
 
+""" TEST PARAMS
+folder = E:\Github\Python\Correlation\test_images\cl
+folder_out = E:\Github\Python\Correlation\test_images\cl\overall_intensity
+"""
+
+""" LOG
+Tue Jun 23 20:45:52 2020 // image 100-2 computed
+Tue Jun 23 20:45:52 2020 // image 40-2 computed
+Tue Jun 23 20:45:52 2020 // image 60-2 computed
+Tue Jun 23 20:45:52 2020 // image 80-2 computed
+"""
