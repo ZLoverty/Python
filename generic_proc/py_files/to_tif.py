14f9cbd8732ca0dd783acd9c64cb4246eda177ca
Zhengyang Liu <liuzy19911112@gmail.com>
2020-08-16 16:31:24 -0500

    illumination correction
    add illumination correction

--- generic_proc/py_files/to_tif.py
+++ generic_proc/py_files/to_tif.py
@@ -1,36 +1,60 @@
 from skimage import io
-import numpy as np
+# import numpy as np
 from nd2reader import ND2Reader
-import matplotlib.pyplot as plt
-import math
-import pdb
-from scipy import fftpack
-from myImageLib import dirrec, to8bit, bpass
-import trackpy as tp
 import time
 import os
 import sys
+
+def illumination_correction(img, avg):
+    """
+    Correct the illumination inhomogeneity in microscope images.
+    
+    Args:
+    img -- input image with illumination inhomogeneity
+    avg -- average of (a large number of) raw images
+    
+    Returns:
+    corrected -- corrected image
+    """
+    corrected = (img / avg * img.mean() / (img / avg).mean()).astype('uint8')
+    return corrected
 
 nd2Dir = sys.argv[1]
 folder, file = os.path.split(nd2Dir)
 
 name, ext = os.path.splitext(file)
 saveDir = os.path.join(folder, name)
-# saveDir16 = os.path.join(folder, name, '16-bit')
 saveDir8 = os.path.join(folder, name, '8-bit')
-# if os.path.exists(saveDir16) == False:
-    # os.makedirs(saveDir16)
 if os.path.exists(saveDir8) == False:
-    os.makedirs(saveDir8)
-    
+    os.makedirs(saveDir8)    
 with open(os.path.join(saveDir, 'log.txt'), 'w') as f:
-    pass
+    f.write('nd2Dir = ' + str(nd2Dir) + '\n')
+    
+# Compute average
+# to minimize the memory needed, I first loop over an nd2 file to get the average
+# Then loop one more time to compute the output
+
+threshold = 100
+
+with ND2Reader(nd2Dir) as images:
+    count = 0
+    for num, image in enumerate(images):
+        if image.mean() > threshold: # exclude light-off images, typically in kinetics experiment
+            count += 1
+            if count == 1:
+                avg = image.astype('float64')
+            else:
+                avg += image
+    avg = avg / count / 8          
     
 with ND2Reader(nd2Dir) as images:
     for num, image in enumerate(images):
         img8 = (image/2**3).astype('uint8')
-        # io.imsave(os.path.join(saveDir16, '%04d.tif' % num), image)
-        io.imsave(os.path.join(saveDir8, '%04d.tif' % num), img8)
+        if image.mean() > threshold:
+            corrected = illumination_correction(img8, avg)
+        else:
+            corrected = img8
+        io.imsave(os.path.join(saveDir8, '%04d.tif' % num), corrected)
         with open(os.path.join(saveDir, 'log.txt'), 'a') as f:
             f.write(time.asctime() + ' // Frame {0:04d} converted\n'.format(num))
         
@@ -41,6 +65,8 @@ This script does not apply auto-contrast and save both 16-bit and 8-bit images.
 
 - Edit:
 06162020 - No longer export 16-bit images.
+08162020 - 1. write input arguments in log.txt
+           2. (important) add illumination correction. All frames will be corrected according to the whole video.
 """ 
 
 """ SYNTAX
@@ -48,7 +74,7 @@ python to_tif.py nd2Dir
 """
 
 """ TEST PARAMS
-nd2Dir = I:\Github\Python\generic_proc\test_images\test.nd2
+nd2Dir = E:\Github\Python\generic_proc\test_images\test.nd2
 """
 
 """ LOG
