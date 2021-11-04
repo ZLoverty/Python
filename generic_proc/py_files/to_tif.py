69b11c97dfe4c947a6ec035851208b4f58c1a6eb
Zhengyang Liu <liuzy19911112@gmail.com>
2021-11-04 12:54:04 +0100

    Modify to_tif
    1. Set `check_contrast` to False to avoid CLI spamming
    2. Add 'exp1' before the image number, in accordance to Cristian's image 
    naming convention

--- generic_proc/py_files/to_tif.py
+++ generic_proc/py_files/to_tif.py
@@ -8,11 +8,11 @@ import sys
 def illumination_correction(img, avg):
     """
     Correct the illumination inhomogeneity in microscope images.
-    
+
     Args:
     img -- input image with illumination inhomogeneity
     avg -- average of (a large number of) raw images
-    
+
     Returns:
     corrected -- corrected image
     """
@@ -30,11 +30,11 @@ folder, file = os.path.split(nd2Dir)
 name, ext = os.path.splitext(file)
 saveDir = os.path.join(folder, name)
 saveDir8 = os.path.join(folder, name, '8-bit')
-if os.path.exists(saveDir8) == False:
-    os.makedirs(saveDir8)    
+if os.path.exists(saveDir) == False:
+    os.makedirs(saveDir)
 with open(os.path.join(saveDir, 'log.txt'), 'w') as f:
     f.write('nd2Dir = ' + str(nd2Dir) + '\n')
-    
+
 # Compute average
 # to minimize the memory needed, I first loop over an nd2 file to get the average
 # Then loop one more time to compute the output
@@ -50,19 +50,19 @@ if remove == True:
                     avg = image.astype('float64')
                 else:
                     avg += image
-        avg = avg / count / 8          
-    
+        avg = avg / count / 8
+
 with ND2Reader(nd2Dir) as images:
     for num, image in enumerate(images):
-        img8 = (image/2**3).astype('uint8')
+        # img8 = (image/2**3).astype('uint8')
         if image.mean() > threshold and remove == True:
-            corrected = illumination_correction(img8, avg)
+            corrected = illumination_correction(image, avg)
         else:
-            corrected = img8
-        io.imsave(os.path.join(saveDir8, '%04d.tif' % num), corrected)
+            corrected = image
+        io.imsave(os.path.join(saveDir, 'exp1%05d.tif' % num), corrected, check_contrast=False)
         with open(os.path.join(saveDir, 'log.txt'), 'a') as f:
             f.write(time.asctime() + ' // Frame {0:04d} converted\n'.format(num))
-        
+
 """ DESCRIPTION
 Convert *.nd2 file to image sequence of 8-bit grayscale images. Save this image sequence in a subfolder under the same folder as the *.nd2 file with corresponding name as the *.nd2 file name.
 
@@ -73,7 +73,10 @@ This script does not apply auto-contrast and save both 16-bit and 8-bit images.
 08162020 - 1. write input arguments in log.txt
            2. (important) add illumination correction. All frames will be corrected according to the whole video.
 08182020 - Add argument 'remove', determining if background subtraction is applied or not. Default to False.
-""" 
+10282021 - Remove "8-bit" folder, export original images instead of converting to 8-bit
+11042021 - 1. Set `check_contrast` to False to avoid CLI spamming
+           2. Add 'exp1' before the image number, in accordance to Cristian's image naming convention
+"""
 
 """ SYNTAX
 python to_tif.py nd2Dir remove
@@ -86,7 +89,3 @@ nd2Dir = E:\Github\Python\generic_proc\test_images\test.nd2
 """ LOG
 Tue Jan 14 20:54:03 2020 // Frame 00000 converted
 """
-
-
-
-    
