ff494cbe49bb02436592edc5f187961e825d203e
Zhengyang Liu <liuzy19911112@gmail.com>
2021-12-15 15:09:39 +0100

    Add PIV_masked function

--- mylib/pivLib.py
+++ mylib/pivLib.py
@@ -18,28 +18,11 @@ def PIV1(I0, I1, winsize, overlap, dt, smooth=True):
     else:
         frame_data = pd.DataFrame(data=np.array([x.flatten(), y.flatten(), u0.flatten(), v0.flatten()]).T, columns=['x', 'y', 'u', 'v'])
     return frame_data
-    
-def imseqPIV(folder, winsize, overlap, dt):       
-    data = pd.DataFrame()
-    l = readseq(folder)
-    for num, i in l.iterrows():
-        # read 2 adjacent images
-        if num % 2 == 0:
-            I0 = io.imread(i.Dir)
-            continue 
-        I1 = io.imread(i.Dir)
-        # run PIV function "extended_search_area_piv()"
-        frame_data = PIV1(I0, I1, winsize, overlap, dt)
-        if num < 2:
-            data = frame_data.assign(frame=num)
-        else:
-            data = data.append(frame_data.assign(frame=num))
-    return data
 
 def read_piv(pivDir):
     """
     Read piv data from pivDir as X, Y, U, V
-    
+
     X, Y, U, V = read_piv(pivDir)
     """
     pivData = pd.read_csv(pivDir)
@@ -50,7 +33,86 @@ def read_piv(pivDir):
     U = np.array(pivData.u).reshape((row, col))
     V = np.array(pivData.v).reshape((row, col))
     return X, Y, U, V
-    
+
+def PIV(I0, I1, winsize, overlap, dt):
+    """ Normal PIV """
+    u0, v0, sig2noise = pyprocess.extended_search_area_piv(
+        I0.astype(np.int32),
+        I1.astype(np.int32),
+        window_size=winsize,
+        overlap=overlap,
+        dt=dt,
+        search_area_size=winsize,
+        sig2noise_method='peak2peak',
+    )
+    # get x, y
+    x, y = pyprocess.get_coordinates(
+        image_size=I0.shape,
+        search_area_size=winsize,
+        overlap=overlap,
+        window_size=winsize
+    )
+    u1, v1, mask_s2n = validation.sig2noise_val(
+        u0, v0,
+        sig2noise,
+        threshold = 1.05,
+    )
+    # replace_outliers
+    u2, v2 = filters.replace_outliers(
+        u1, v1,
+        method='localmean',
+        max_iter=3,
+        kernel_size=3,
+    )
+    # median filter smoothing
+    u3 = medfilt2d(u2, 3)
+    v3 = medfilt2d(v2, 3)
+    return x, y, u3, v3
+
+def PIV_masked(I0, I1, winsize, overlap, dt, mask):
+    """Apply PIV analysis on masked images
+    Args:
+    I0, I1 -- adjacent images in a sequence
+    winsize, overlap, dt -- PIV parameters
+    mask -- a boolean array, False marks masked region and True marks the region of interest
+    mask_procedure -- the option chosen to apply the mask, used for testing, remove in the future.
+    Returns:
+    frame_data -- x, y, u, v DataFrame, here x, y is wrt original image, (u, v) are in px/s
+
+    This function is rewritten based on the PIV_droplet() function in piv_droplet.py script.
+    The intended usage is just to pass one additional `mask` parameter, on top of conventional parameter set.
+
+    EDIT
+    ====
+    Dec 14, 2021 -- Initial commit.
+    Dec 15, 2021 -- After testing 2 masking procedure, option 1 is better.
+                    Two procedures produce similar results, but option 1 is faster.
+                    So this function temporarily uses option 1, until a better procedure comes.
+
+    MASKING PROCEDURE
+    =================
+    Option 1:
+    i) Mask on raw image: I * mask, perform PIV
+    ii) Divide mask into windows: mask_w
+    iii) use mask_w to mask resulting velocity field: u[~mask_w] = np.nan
+    ---
+    Option 2:
+    i) Perform PIV on raw images
+    ii) Divide mask into windows:mask_w
+    iii) use mask_w to mask resulting velocity field: u[~mask_w] = np.nan
+    ---
+    """
+    assert(mask.shape==I0.shape)
+    mask = mask >= mask.mean() # convert mask to boolean array
+    I0_mask = I0 * mask
+    I1_mask = I1 * mask
+    x, y, u, v = PIV(I0_mask, I1_mask, winsize, overlap, dt)
+    mask_w = divide_windows(mask, windowsize=[winsize, winsize], step=winsize-overlap)[2] >= 1
+    assert(mask_w.shape==x.shape)
+    u[~mask_w] = np.nan
+    v[~mask_w] = np.nan
+    return x, y, u, v
+
 if __name__ == '__main__':
     # set PIV parameters
     winsize = 50 # pixels
