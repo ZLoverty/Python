cfb4e378b3349d1de56d10aa23cc3e507d2d250a
Zhengyang Liu <liuzy19911112@gmail.com>
2022-03-15 10:45:29 +0100

    rawImage class for exporting tif

--- mylib/myImageLib.py
+++ mylib/myImageLib.py
@@ -5,6 +5,13 @@ from scipy.signal import medfilt2d, convolve2d, fftconvolve
 from scipy.optimize import curve_fit
 from scipy import exp
 import pandas as pd
+import sys
+import psutil
+from skimage import io
+import time
+import shutil
+from nd2reader import ND2Reader
+# %% codecell
 
 def dirrec(path, filename):
     """
@@ -35,7 +42,6 @@ def dirrec(path, filename):
             elif file == filename:
                 dirList.append(os.path.join(r, file))
     return dirList
-
 def to8bit(img16):
     """
     Enhance contrast and convert to 8-bit
@@ -46,7 +52,6 @@ def to8bit(img16):
     minn = img16.min()
     img8 = (img16 - minn) / (maxx - minn) * 255
     return img8.astype('uint8')
-
 def bpass(*args):
     img8 = args[0]
     low = args[1]
@@ -68,12 +73,10 @@ def bpass(*args):
     im_new = im_new - im_new.min()
     im_new = np.floor_divide(im_new, (im_new.max()+1)/256)
     return im_new.astype('uint8')
-
 def bestcolor(n):
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
               '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
     return colors[n]
-
 def wowcolor(n):
     colors = ['#C41F3B', '#A330C9', '#FF7D0A', '#A9D271', '#40C7EB',
               '#00FF96', '#F58CBA', '#FFF569', '#0070DE', '#8787ED',
@@ -81,7 +84,6 @@ def wowcolor(n):
               '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
               '#bcbd22', '#17becf']
     return colors[n]
-
 def matlab_style_gauss2D(shape=(3,3),sigma=0.5):
     """
     2D gaussian mask - should give the same result as MATLAB's
@@ -95,7 +97,6 @@ def matlab_style_gauss2D(shape=(3,3),sigma=0.5):
     if sumh != 0:
         h /= sumh
     return h
-
 def FastPeakFind(data):
     if str(data.dtype) != 'float32':
         data = data.astype('float32')
@@ -128,7 +129,6 @@ def FastPeakFind(data):
             cent.append(xy)
     cent = np.asarray(cent).transpose()
     return cent
-
 def minimal_peakfind(img):
     edg = 3
     shape = img.shape
@@ -149,7 +149,6 @@ def minimal_peakfind(img):
             cent.append(xy)
     cent = np.asarray(cent).transpose()
     return cent
-
 def maxk(array, num_max):
     array = np.asarray(array)
     length = array.size
@@ -157,7 +156,6 @@ def maxk(array, num_max):
     idx = np.argsort(array)
     idx2 = np.flip(idx)
     return idx2[0, 0: num_max]
-
 def track_spheres_dt(img, num_particles):
     def gauss1(x,a,x0,sigma):
         return a*exp(-(x-x0)**2/(2*sigma**2))
@@ -185,10 +183,8 @@ def track_spheres_dt(img, num_particles):
             max_coor[:, num] = max_coor_tmp[:, num]
             continue
     return max_coor, pk_value
-
 def gauss1(x,a,x0,sigma):
     return a*exp(-(x-x0)**2/(2*sigma**2))
-
 def show_progress(progress, label='', bar_length=60):
     """ Display a progress bar
     Args:
@@ -205,7 +201,6 @@ def show_progress(progress, label='', bar_length=60):
     N_finish = int(progress*bar_length)
     N_unfinish = bar_length - N_finish
     print('{0} [{1}{2}] {3:.1f}%'.format(label, '#'*N_finish, '-'*N_unfinish, progress*100))
-
 def readdata(folder, ext='csv'):
     """
     Read data files with given extensions in a folder.
@@ -232,3 +227,152 @@ def readdata(folder, ext='csv'):
     fileList = fileList.assign(Name=nameList, Dir=dirList)
     fileList = fileList.sort_values(by=['Name']).reset_index(drop=True)
     return fileList
+
+# %% codecell
+
+class rawImage:
+    """Implement raw image converting schemes."""
+    def __init__(self, file_dir):
+        self.file = file_dir
+    def extract_tif(self):
+        """Wrapper of all format-specific extractors."""
+        file, ext = os.path.splitext(self.file)
+        if ext == ".raw":
+            self.extract_raw()
+        elif ext == ".nd2":
+            self.extract_nd2()
+        else:
+            raise ValueError("Unrecognized image format {}".format(ext))
+    def extract_raw(self, cutoff=None):
+        """Extract .tif sequence from .raw file.
+        cutoff -- number of images extracted.
+        Edit:
+        Mar 14, 2022 -- Initial commit."""
+        # read info from info_file
+        folder, file = os.path.split(self.file)
+        info_file = os.path.join(folder, "RawImageInfo.txt")
+        if os.path.exists(info_file):
+            fps, h, w = self.read_raw_image_info(info_file)
+        else:
+            print("Info file missing!")
+
+        # create output folders, skip if both folders exist
+        save_folder = folder
+        out_raw_folder = os.path.join(save_folder, 'raw')
+        out_8_folder = os.path.join(save_folder, '8-bit')
+        if os.path.exists(out_raw_folder) and os.path.exists(out_8_folder):
+            print(time.asctime() + " // {} tif folders exists, skipping".format(self.file))
+            return None
+        if os.path.exists(out_raw_folder) == False:
+            os.makedirs(out_raw_folder)
+        if os.path.exists(out_8_folder) == False:
+            os.makedirs(out_8_folder)
+
+        # check disk
+        if self._disk_capacity_check() == False:
+            raise SystemError("No enough disk capacity!")
+
+        # calculate buffer size based on available memory, use half of it
+        file_size = os.path.getsize(self.file)
+        avail_mem = psutil.virtual_memory().available
+        unit_size = (h * w + 2) * 2 # the size of a single unit in bytes (frame# + image)
+        buffer_size = ((avail_mem // 2) // unit_size) * unit_size # integer number of units
+        remaining_size = file_size
+
+        # print("Memory information:")
+        # print("Available memory: {:.1f} G".format(avail_mem / 2 ** 30))
+        # print("File size: {:.1f} G".format(file_size / 2 ** 30))
+        # print("Buffer size: {:.1f} G".format(buffer_size / 2 ** 30))
+
+        # read the binary files, in partitions if needed
+        num = 0
+        while remaining_size > 0:
+            # load np.array from buffer
+            if remaining_size > buffer_size:
+                read_size = buffer_size
+            else:
+                read_size = remaining_size
+            with open(self.file, "rb") as f:
+                f.seek(-remaining_size, 2) # offset bin file reading, counting remaining size from EOF
+                bytestring = f.read(read_size)
+                a = np.frombuffer(bytestring, dtype=np.uint16)
+            remaining_size -= read_size
+
+            # manipulate the np.array
+            assert(a.shape[0] % (h*w+2) == 0)
+            num_images = a.shape[0] // (h*w+2)
+            img_in_row = a.reshape(num_images, h*w+2)
+            labels = img_in_row[:, :2] # not in use currently
+            images = img_in_row[:, 2:]
+            images_reshape = images.reshape(num_images, h, w)
+
+            # save the image sequence
+            for label, img in zip(labels, images_reshape):
+                # num = label[0] + label[1] * 2 ** 16 + 1 # convert image label to uint32 to match the info in StagePosition.txt
+                io.imsave(os.path.join(save_folder, 'raw', '{:05d}.tif'.format(num)), img, check_contrast=False)
+                io.imsave(os.path.join(save_folder, '8-bit', '{:05d}.tif'.format(num)), to8bit(img), check_contrast=False)
+
+                # with open(os.path.join(out_raw_folder, 'log.txt'), 'a') as f:
+                #     f.write(time.asctime() + ' // frame {:05d}'.format(num))
+                # with open(os.path.join(out_8_folder, 'log.txt'), 'a') as f:
+                #     f.write(time.asctime() + ' // frame {:05d}'.format(num))
+                num += 1
+                if cutoff is not None:
+                    if num > cutoff:
+                        return None
+    def read_raw_image_info(self, info_file):
+        """Read image info, such as fps and image dimensions, from RawImageInfo.txt.
+        Helper function of extract_raw()."""
+        with open(info_file, 'r') as f:
+            a = f.read()
+        fps, h, w = a.split('\n')[0:3]
+        return int(fps), int(h), int(w)
+    def extract_nd2(self):
+        # check disk
+        if self._disk_capacity_check() == False:
+            raise SystemError("No enough disk capacity!")
+
+        folder, file = os.path.split(self.file)
+        name, ext = os.path.splitext(file)
+        saveDir = os.path.join(folder, name, 'raw')
+        saveDir8 = os.path.join(folder, name, '8-bit')
+        if os.path.exists(saveDir) == False:
+            os.makedirs(saveDir)
+        if os.path.exists(saveDir8) == False:
+            os.makedirs(saveDir8)
+        # with open(os.path.join(saveDir, 'log.txt'), 'w') as f:
+        #     f.write('nd2Dir = ' + str(nd2Dir) + '\n')
+        # with open(os.path.join(saveDir8, 'log.txt'), 'w') as f:
+        #     f.write('nd2Dir = ' + str(nd2Dir) + '\n')
+
+        with ND2Reader(self.file) as images:
+            for num, image in enumerate(images):
+                # img8 = (image/2**3).astype('uint8')
+                # 8-bit image now are only used for visualization, i.e. convert to videos
+                # therefore, they are autocontrasted on each frame.
+                io.imsave(os.path.join(saveDir8, '{:05d}.tif'.format(num)), to8bit(image))
+                # if image.mean() > threshold and remove == True:
+                #     corrected = illumination_correction(image, avg)
+                # else:
+                #     corrected = image
+                io.imsave(os.path.join(saveDir, '%05d.tif' % num), image, check_contrast=False)
+                # with open(os.path.join(saveDir, 'log.txt'), 'a') as f:
+                #     f.write(time.asctime() + ' // Frame {0:04d} converted\n'.format(num))
+    def _disk_capacity_check(self):
+        """Check if the capacity of disk is larger than twice of the file size.
+        Args:
+        file -- directory of the (.nd2) file being converted
+        Returns:
+        flag -- bool, True if capacity is enough."""
+        d = os.path.split(self.file)[0]
+        fs = os.path.getsize(self.file) / 2**30
+        ds = shutil.disk_usage(d)[2] / 2**30
+        print("File size {0:.1f} GB, Disk size {1:.1f} GB".format(fs, ds))
+        return ds > 2 * fs
+# %% codecell
+if __name__=="__main__":
+    # %% codecell
+    # test rawImage class
+    file = r"D:\swimming_droplets\2022-03-11_09h55m01s\RawImage.raw"
+    raw = rawImage(file)
+    raw.extract_raw()
